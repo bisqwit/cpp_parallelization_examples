24fbd49bf5aeb26a12b45562f4fbd2fb94715e8a
Joel Yliluoma <joel.yliluoma@iki.fi>
2017-06-09 13:35:13 +0300

    Fix compilation on non-AVX platforms

--- simd_emu.hh
+++ simd_emu.hh
@@ -38,7 +38,7 @@ union my256 { struct { __m128d d[2]; }; struct { __m128i i[2]; };
 #define _mm256_set1_pd(x)              ([](__m128d v){ return my256(v,v); })(_mm_set1_pd(x))
 #define _mm256_set1_epi32(x)           ([](__m128i v){ return my256(v,v); })(_mm_set1_epi32(x))
 #define _mm256_set1_epi64x(x)          ([](__m128i v){ return my256(v,v); })(_mm_set1_epi64x(x))
-#define _mm256_setzero_si256()         ([](__m128i v){ return my256(v,v); })(_mm_setzero_si128())
+#define _mm256_setzero_si256()         my256(_mm_setzero_si128(), _mm_setzero_si128())
 #define _mm256_setzero_pd()            my256(_mm_setzero_pd(),_mm_setzero_pd())
 #define _mm256_set_pd(d,c,b,a)         my256(_mm_set_pd(d,c),_mm_set_pd(b,a))
 #define _mm256_set_epi64x(d,c,b,a)     my256(_mm_set_epi64x(d,c),_mm_set_epi64x(b,a))
