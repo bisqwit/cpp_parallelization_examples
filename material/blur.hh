64f3cb3f3a527926502126ebee61d311e46d936c
Joel Yliluoma <joel.yliluoma@iki.fi>
2017-06-16 09:11:41 +0300

    Documentation

--- material/blur.hh
+++ material/blur.hh
@@ -29,24 +29,28 @@ void blur(const elem_t* input, elem_t* output, elem_t* temp,
         unsigned r = ((n<m ? wl : wu) - 1)/2; // IDK should this be float?
         // boxBlur_4:
         float iarr = 1.f / (r+r+1);
-        // boxBlurH_4:
+        // boxBlurH_4 (blur horizontally for each row):
         const elem_t* scl = data; elem_t* tcl = temp;
         for(unsigned i=0; i<h; ++i)
         {
             auto ti = i*w, li = ti, ri = ti+r;
-            auto fv = scl[ti], lv = scl[ti+w-1]; int val = (r+1)*fv;
+            auto fv = scl[ti], lv = scl[ti+w-1]; int val = 0;
+            #pragma omp simd reduction(+:val)
             for(unsigned j=0; j<r; j++) val += scl[ti+j];
+            val += (r+1)*fv;
             for(unsigned j=0  ; j<=r ; j++) { val += scl[ri++] - fv       ;   tcl[ti++] = std::round(val*iarr); }
             for(unsigned j=r+1; j<w-r; j++) { val += scl[ri++] - scl[li++];   tcl[ti++] = std::round(val*iarr); }
             for(unsigned j=w-r; j<w  ; j++) { val += lv        - scl[li++];   tcl[ti++] = std::round(val*iarr); }
         }
-        // boxBlurT_4:
+        // boxBlurT_4 (blur vertically for each column)
         scl = temp; tcl = output;
         for(unsigned i=0; i<w; ++i)
         {
             auto ti = i, li = ti, ri = ti+r*w;
-            auto fv = scl[ti], lv = scl[ti+w*(h-1)]; int val = (r+1)*fv;
+            auto fv = scl[ti], lv = scl[ti+w*(h-1)]; int val = 0;
+            #pragma omp simd reduction(+:val)
             for(unsigned j=0; j<r;  ++j) val += scl[ti + j*w];
+            val += (r+1)*fv;
             for(unsigned j=0; j<=r; ++j)    { val += scl[ri] - fv     ;  tcl[ti] = std::round(val*iarr);  ri+=w; ti+=w; }
             for(unsigned j=r+1; j<h-r; ++j) { val += scl[ri] - scl[li];  tcl[ti] = std::round(val*iarr);  li+=w; ri+=w; ti+=w; }
             for(unsigned j=h-r; j<h; ++j)   { val += lv      - scl[li];  tcl[ti] = std::round(val*iarr);  li+=w; ti+=w; }
